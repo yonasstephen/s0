fd56f680ed14266edbfeb06c37f793b70a01e41b
yonasstephen <yonas.stephen@gmail.com>
2021-08-01 20:49:34 +0800

    Handle graceful server shutdown

--- server/http.go
+++ server/http.go
@@ -1,1 +1,78 @@
 package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"github.com/yonasstephen/s0/store/handler"
+)
+
+// Server is the server that serves API endpoints
+type HTTPServer struct {
+	config Config
+}
+
+// NewServer instantiate a new server with the given config
+func NewHTTPServer(config Config) *HTTPServer {
+	return &Server{
+		config: config,
+	}
+}
+
+// Start sets up the server routing & start the server.
+// will throw panic if the server fails to start
+func (s *Server) Start() {
+	// setup static directory
+	fs := http.FileServer(http.Dir("./static"))
+
+	// setup router
+	r := mux.NewRouter()
+	r.HandleFunc("/upload", handler.UploadHandler).Methods(http.MethodPost)
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         fmt.Sprintf("127.0.0.1:%d", s.config.Port),
+		WriteTimeout: s.config.WriteTimeout,
+		ReadTimeout:  s.config.ReadTimeout,
+	}
+
+	logLevel, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		log.Printf("invalid log level '%s', defaulting to error level", s.config.LogLevel)
+		log.Printf("valid log levels are: panic, fatal, error, info, warn, debug, trace")
+		logrus.SetLevel(logrus.ErrorLevel)
+	} else {
+		logrus.SetLevel(logLevel)
+	}
+
+	shutdownChan := make(chan os.Signal, 1)
+	signal.Notify(shutdownChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		logrus.Infof("Running s0 on port %d...", s.config.Port)
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			logrus.Fatal(err)
+		}
+	}()
+
+	logrus.Info("Server started")
+
+	<-shutdownChan
+	logrus.Info("Server stopped")
+
+	ctx, cancel := context.ContextWithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Shutdown failed:", err)
+	}
+	logrus.Info("Server stopped gracefully")
+}
--- server/server.go
+++ server/server.go
@@ -1,14 +1,8 @@
 package server
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"context"
 	"time"
-
-	"github.com/gorilla/mux"
-	"github.com/sirupsen/logrus"
-	"github.com/yonasstephen/s0/store/handler"
 )
 
 // Config stores configuration for server
@@ -19,45 +13,7 @@ type Config struct {
 	WriteTimeout time.Duration
 }
 
-// Server is the server that serves API endpoints
-type Server struct {
-	config Config
-}
-
-// NewServer instantiate a new server with the given config
-func NewServer(config Config) *Server {
-	return &Server{
-		config: config,
-	}
-}
-
-// Start sets up the server routing & start the server.
-// will throw panic if the server fails to start
-func (s *Server) Start() {
-	// setup static directory
-	fs := http.FileServer(http.Dir("./static"))
-
-	// setup router
-	r := mux.NewRouter()
-	r.HandleFunc("/upload", handler.UploadHandler).Methods(http.MethodPost)
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
-
-	srv := &http.Server{
-		Handler:      r,
-		Addr:         fmt.Sprintf("127.0.0.1:%d", s.config.Port),
-		WriteTimeout: s.config.WriteTimeout,
-		ReadTimeout:  s.config.ReadTimeout,
-	}
-
-	logLevel, err := logrus.ParseLevel(s.config.LogLevel)
-	if err != nil {
-		log.Printf("invalid log level '%s', defaulting to error level", s.config.LogLevel)
-		log.Printf("valid log levels are: panic, fatal, error, info, warn, debug, trace")
-		logrus.SetLevel(logrus.ErrorLevel)
-	} else {
-		logrus.SetLevel(logLevel)
-	}
-
-	log.Printf("Running s0 on port %d...", s.config.Port)
-	log.Fatal(srv.ListenAndServe())
+type Server interface {
+	Start()
+	Shutdown(ctx context.Context) error
 }
